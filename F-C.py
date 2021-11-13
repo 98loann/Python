0c0afaee4253160ccf815a1554ae34ab87b10a02
98loann <86321565+98loann@users.noreply.github.com>
2021-11-13 15:52:38 -0300

    Add files via upload

---
+++ F-C.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+print("Calculadora de Farenheit para Celsius :D")
+
+f=float(input("Qual a temperatura em Farenheit? "))
+
+c=f-32/1.8
+
+print("A temperatura em Celsius eh de ",c,"°C")
