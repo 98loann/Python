3d2b56d03cb49090aee3a76634abf2dd81093acb
98loann <86321565+98loann@users.noreply.github.com>
2021-11-13 15:49:57 -0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+print("Calculadora de numeros inteiros")
+
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+
+print(num1+num2)
