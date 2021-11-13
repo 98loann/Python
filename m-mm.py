4c009ad5299841aefc4e88e7ad2b78fa27f77f65
98loann <86321565+98loann@users.noreply.github.com>
2021-11-13 15:50:20 -0300

    Add files via upload

---
+++ m-mm.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+metros = float(input("Qual o valor em metros? "))
+print("O valor convertido e: ")
+print(metros*1000)
+
