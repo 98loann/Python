18d24cd252c946537cd7b9beac8eef70b83ea673
98loann <86321565+98loann@users.noreply.github.com>
2021-11-13 15:52:14 -0300

    Add files via upload

---
+++ velmedia.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+distancia = float(input("Qual a distancia, em KM, da viagem? "))
+vel_media = float(input("Qual sera a velocidade media? "))
+
+print("O tempo de viagem levara por volta de ", distancia/vel_media," horas")
