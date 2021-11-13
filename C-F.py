b931abab97222185a5b3efd961879180a7d4558c
98loann <86321565+98loann@users.noreply.github.com>
2021-11-13 15:52:28 -0300

    Add files via upload

---
+++ C-F.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+print("TRANSFORMADO DE CELSIUS PRA FARENHEIT")
+
+c=float(input("Qual a temperatura em graus celsius? "))
+
+F = 9*c/5 + 32
+
+print("A temperatura em Farenheit eh de ", F,'F')
