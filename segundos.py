1f8e8316cb2c1d9ee33e5ee42ff2ca060f3b35f5
98loann <86321565+98loann@users.noreply.github.com>
2021-11-13 15:51:15 -0300

    Add files via upload

---
+++ segundos.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+dias = float(input("Digite a quantidade de dias: "))
+dia = dias * 86400
+horas = float(input("Digite a quantidade de horas: "))
+hora = horas * 3600
+minutos = float(input("Digite a quantidade de minutos: "))
+minuto = minutos * 60
+segundos = float(input("Digite a quantidade de segundos: "))
+
+print("O total em segundos eh de: ",dia+hora+minuto+segundos)
